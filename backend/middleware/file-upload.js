bf50e969ee342a03dd5a8672cdc33608da3cb6af
Boyan <btomov97@gmail.com>
2020-10-03 15:30:20 +0300

    Make separate directories for user and place pictures

--- backend/middleware/file-upload.js
+++ backend/middleware/file-upload.js
@@ -1,28 +1,33 @@
-const multer = require('multer');
-const uuid = require('uuid/v1');
+const multer = require("multer");
+const uuid = require("uuid/v1");
 
 const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpeg',
-  'image/jpg': 'jpg'
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
 };
 
 const fileUpload = multer({
   limits: 500000,
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, 'uploads/images');
+      //Easy way to check if its a place or a user
+      if (req.body.address) {
+        cb(null, "uploads/images/places");
+      } else {
+        cb(null, "uploads/images/users");
+      }
     },
     filename: (req, file, cb) => {
       const ext = MIME_TYPE_MAP[file.mimetype];
-      cb(null, uuid() + '.' + ext);
-    }
+      cb(null, uuid() + "." + ext);
+    },
   }),
   fileFilter: (req, file, cb) => {
     const isValid = !!MIME_TYPE_MAP[file.mimetype];
-    let error = isValid ? null : new Error('Invalid mime type!');
+    let error = isValid ? null : new Error("Invalid mime type!");
     cb(error, isValid);
-  }
+  },
 });
 
 module.exports = fileUpload;
